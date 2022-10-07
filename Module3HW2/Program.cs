c30f35b292bf5bbc123c8a083b0481bfde46d748
Artem Kolomiiets <akolomiets02@gmail.com>
2022-10-07 20:34:06 +0300

    init + stylecop

---
+++ Module3HW2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module3HW2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
